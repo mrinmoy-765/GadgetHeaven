bbd0d7c97808a8d01e0c9413e83af827b1def53f
mrinmoy-765 <mrinmoyd629@gmail.com>
2025-02-23 17:08:37 +0600

    page not found

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BsEmojiFrown } from "react-icons/bs";
 
 const ErrorPage = () => {
   return (
-    <div>
-        <h1>404 Not Found</h1>
-    </div>
-  )
-}
+    <div className="flex w-full h-screen flex-col lg:flex-row items-center justify-center">
+      {/* First Div: 3/5 Width */}
+      <div className="card rounded-box grid h-auto flex-grow lg:w-3/5 place-items-center">
+        <BsEmojiFrown className="text-9xl text-gray-500"/>
+        <h1 className="text-9xl text-gray-500 font-[1000]">404</h1>
+        <p className="text-4xl text-gray-500 font-semibold">Page Not Found</p>
+      </div>
 
-export default ErrorPage
+      {/* Thicker Divider */}
+      <div className="divider lg:divider-horizontal"></div>
+
+      {/* Second Div: 2/5 Width */}
+      <div className="card rounded-box grid h-32 flex-grow lg:w-2/5 place-items-center text-center">
+        <p className="text-base font-semibold text-gray-400">
+          The site configured at this address does not contain the requested
+          file
+        </p>
+        <p className="text-base text-gray-400 font-semibold">If this is your site, make sure the filename cases match the URL</p>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
